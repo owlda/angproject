2fff86288e7910cda00efdd3ff08e2054195bfdb
Denis Andrienko <e1895226@cmaisonneuve.qc.ca>
2018-12-24 22:26:54 -0500

    add /cli /sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
@@ -10,8 +11,9 @@ import { CreatePostComponent } from './posts/create-post/create-post.component';
     CreatePostComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/posts/create-post/create-post.component.ts
+++ src/app/posts/create-post/create-post.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html'
 })
 
 export class CreatePostComponent {
+  enteredValue = '';
+  NewPost = 'Input your text here';
+  AddPost() {
+    this.NewPost = this.enteredValue;
+  }
 
 }
