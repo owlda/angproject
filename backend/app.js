e72787aca9cd1af3281a4f11f45de3e2f65e3ec1
owlda <e1895226@cmaisonneuve.qc.ca>
2019-01-02 20:37:12 -0500

    add nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use((request, reponse, next)=>{
-  console.log('express')
-  next();
-})
-
-app.use((request, reponse, next)=>{
-   reponse.send('reponse from express')
+app.use('/posts', (request, reponse, next)=>{
+  const posts = [
+    {id:"09945",
+    title:"first post",
+    content:"message here..."},
+    {id:"099scs",
+    title:"second post",
+    content:"message here...too"}
+  ];
+   reponse.status(200).json({
+     message:"succesfully!",
+     posts:posts
+   });
 })
 
 module.exports = app;
