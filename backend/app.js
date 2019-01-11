f3d5fd9d3181924c4802cc91bbf6198d4c20e68e
owlda <e1895226@cmaisonneuve.qc.ca>
2019-01-11 10:08:16 -0500

    added app.get() methode to fetch the data from Mongo DB

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,20 @@ const express = require('express');
 const app = express();
 const BodyParser = require('body-parser');
 const mongoose = require('mongoose');
-//post model
+//Post model
 const Post = require('./models/post');
-//connection string to MongoDB cluster
+
+//Connection string to MongoDB cluster
 mongoose.connect("mongodb+srv://userdb:96560023_ad@clustermdb-wbnah.mongodb.net/angular-db?retryWrites=true").then(() => {
   console.log('Connection to database was successully.');
 }).catch(() => {
   console.log('Connection to database was failed.');
 });
-//body parser using
+
+//Body parser using
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended:false}));
+
 //application access throught out
 app.use((request, reponse, next)=>{
   reponse.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,33 +25,24 @@ app.use((request, reponse, next)=>{
   next();
 });
 
-// method to save post to db Mongo Atlas
+// Method to save post to db Mongo Atlas
 app.post("/posts", (request, reponse, next)=>{
   const post = new Post({
     title: request.body.title,
     content: request.body.content
   });
   post.save();
-  console.log(post);
   reponse.status(201).json({message:"successfully added the post!"});
 });
 
-app.use('/posts', (request, reponse, next)=>{
-  const posts = [
-    {id:"09945",
-    title:"first post",
-    content:"message here..."},
-    {id:"099scs",
-    title:"second post",
-    content:"message here...too"}
-  ];
-   reponse.status(200).json({
-     message:"successfully!",
-     posts:posts
-   });
+// Method to get post from Mongo db Atlas
+app.get('/posts', (request, reponse, next)=>{
+    // Static methode from mongoose Post
+    Post.find().then(documents => {
+      reponse.status(200).json({
+        message:"successfully!",
+        posts:documents});
+    });
 });
-
-
-
-
+// Export app to out of the class
 module.exports = app;
